8fc46c3a2b492b5b73e16fe3002ade73f98e1711
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-03-30 12:57:00 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+# -- Project information -----------------------------------------------------
+
+project = 'niveristand-manual'
+copyright = '2021, National Instruments'
+author = 'National Instruments'
+
+# The short X.Y version
+version = '0.1'
+# The full version, including alpha/beta/rc tags
+release = '0.1.0 beta'
+
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
