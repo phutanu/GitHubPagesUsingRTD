2a7b25beec2b29acd1e13fd1a98a00a1d31ff98c
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-03-31 10:05:13 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ copyright = '2021, National Instruments'
 author = 'National Instruments'
 
 # The short X.Y version
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags
 release = '0.2.0 beta'
 
