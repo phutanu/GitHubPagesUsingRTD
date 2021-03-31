956abc4976b3965dddb6ba2c994548b1f4de3b15
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-03-31 12:28:27 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,8 +25,8 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'press'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'press'
 
 # We need a markdown parser in Sphinx in order to parse .md files.
 extensions = [
