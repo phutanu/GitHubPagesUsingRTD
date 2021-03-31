cbd18d96c4b56b3c4475c84f7c2b02cf97f80f2a
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-03-31 11:24:33 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 # html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx-veldus-theme'
 
 # We need MyST parser in Sphinx in order to parse .md files.
 extensions = ['recommonmark']
