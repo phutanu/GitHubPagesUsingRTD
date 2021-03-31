2a55ccfcf129018dac7f15598e65cb444c13ce12
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-03-31 12:20:21 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
 ]
 
 # At the bottom of conf.py
-def setup(app):
+# def setup(app):
 app.add_config_value('recommonmark_config', {
         'url_resolver': lambda url: github_doc_root + url,
         'auto_toc_tree_section': 'Contents',
