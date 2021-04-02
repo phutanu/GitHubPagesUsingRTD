e2754f4d7c1f7705cf68e5220e6a9d042b6fd3d3
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-04-02 16:19:59 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,9 +51,9 @@ templates_path = ['_templates']
 master_doc = 'index'
 
 # General information about the project.
-project = u'Recommonmark'
-copyright = u'2015, Luca Barbato, Eric Holscher, and contributors'
-author = u'Luca Barbato, Eric Holscher, and contributors'
+project = u'VeriStand Manual'
+copyright = u'2021 National Instruments'
+author = u'John Doe and contributors'
 
 github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/docs/'
 
@@ -207,7 +207,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Recommonmarkdoc'
+htmlhelp_basename = 'VS Manual'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -229,8 +229,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'Recommonmark.tex', u'Recommonmark Documentation',
-   u'Lu Zero, Eric Holscher, and contributors', 'manual'),
+  (master_doc, 'VS Manual.tex', u'VeriStand Manual',
+   u'John Doe and contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -259,7 +259,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'recommonmark', u'Recommonmark Documentation',
+    (master_doc, 'Page1 Header', 'Page2 Header', u'Recommonmark Documentation',
      [author], 1)
 ]
 
@@ -273,7 +273,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'Recommonmark', u'Recommonmark Documentation',
+  (master_doc, 'Recommonmark', u'VeriStand Manual',
    author, 'Recommonmark', 'One line description of project.',
    'Miscellaneous'),
 ]
