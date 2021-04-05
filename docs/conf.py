30e2451f2e8ee07a641c1d69fef8ed953adea40f
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-04-05 16:45:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('..'))
-import myst-parser
+import myst_parser
 #from recommonmark.transform import AutoStructify
 
 
