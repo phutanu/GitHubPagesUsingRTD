a6dbba24c430187d6b8337e5e2816f197857a4f3
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-09-28 14:13:28 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -294,11 +294,7 @@ texinfo_documents = [
 def setup(app):
     app.add_config_value('recommonmark_config', {
         #'url_resolver': lambda url: github_doc_root + url,
-        'auto_toc_tree_section': 'VeriStand Manual',
         'auto_toc_maxdepth': 4,
-        'enable_math': False,
-        'enable_inline_math': False,
         'enable_eval_rst': True,
-        'enable_auto_doc_ref': True,
     }, True)
     app.add_transform(AutoStructify)
