054ab37215528ff2211c2a033a49b54225507725
Paul Huțanu <62251693+phutanu@users.noreply.github.com>
2021-09-28 14:05:12 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -294,7 +294,7 @@ texinfo_documents = [
 def setup(app):
     app.add_config_value('recommonmark_config', {
         #'url_resolver': lambda url: github_doc_root + url,
-        'auto_toc_tree_section': 'Index',
+        'auto_toc_tree_section': 'VeriStand Manual',
         'auto_toc_maxdepth': 4,
         'enable_math': False,
         'enable_inline_math': False,
